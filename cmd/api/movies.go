5a105cdb0c1a399d0106222c124ee2d8d8a578fa
Oscar GS <oscargs.dev@gmail.com>
2026-01-06 08:31:51 -0600

    Validation for filters

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -187,8 +187,9 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	input.Page = app.readInt(qs, "page", 1, v)
 	input.PageSize = app.readInt(qs, "page_size", 20, v)
 	input.Sort = app.readString(qs, "sort", "id")
+	input.SortSafelist = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
 
-	if !v.Valid() {
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
